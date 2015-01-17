5394f5c6aa66686da92ee6554cc1d4e18a7e848d
Ketonia <ketevanzazarashvili.kz@gmail.com>
2015-01-17 20:59:40 +0400

    Add Question method

--- main.rb
+++ main.rb
@@ -1,16 +1,33 @@
 Player = Struct.new(:name)
 
 class Game 
-	$table = Array.new(3, Array.new(3, 'a'))
+	$table = Array.new(3, Array.new(3, ' '))
 	def initialize
 
 		puts "Welcome to the game!"
 
 		@o = Player.new("0")
 		@x = Player.new("X")
+
+		puts "You are 0!"
+		@currentPlayer = @o.name
+
+		self.Question
+
 	end
 
-	public
+
+	def Question
+		self.DrawTable
+		puts "\n\nIt's #{@currentPlayer}'s turn!"
+
+		puts "Where would you like to move? (Ex: A1, A2, etc):"
+
+		answer = gets.chomp
+
+		@currentPlayer = @currentPlayer == @x.name ? @o.name : @x.name
+	end
+
 	def DrawTable
 		counter = 0
 		$table.each do |item|
@@ -22,6 +39,5 @@ class Game
 end
 
 game = Game.new
-game.DrawTable
 
 
