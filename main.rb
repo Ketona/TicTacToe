d991d7fd50de16697f816666b4952cb14fd26e9f
Ketonia <ketevanzazarashvili.kz@gmail.com>
2015-01-17 13:14:41 +0400

    Add DrawTable method

--- main.rb
+++ main.rb
@@ -1,11 +1,27 @@
 Player = Struct.new(:name)
 
 class Game 
+	$table = Array.new(3, Array.new(3, 'a'))
 	def initialize
-		O = Player.new("0")
-		X = Player.new("X")
+
+		puts "Welcome to the game!"
+
+		@o = Player.new("0")
+		@x = Player.new("X")
+	end
+
+	public
+	def DrawTable
+		counter = 0
+		$table.each do |item|
+			print item * '|'
+			counter += 1
+			puts "\n-----" if counter < 3
+		end
 	end
 end
 
+game = Game.new
+game.DrawTable
 
-Game.new
+
