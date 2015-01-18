b3879a208fd0f2d615f2a3ba52f943137c1f0525
Ketonia <ketevanzazarashvili.kz@gmail.com>
2015-01-18 22:53:53 +0400

    Some Syntax Changes

--- game.rb
+++ game.rb
@@ -1,10 +1,11 @@
 Player = Struct.new(:name)
 
 class Game
+
   @@table = Array.new(3){ Array.new(3){' '} }
   @@counter = 1
-  def initialize
 
+  def initialize
     puts 'Welcome to the game!'
 
     @o = Player.new('0')
@@ -13,23 +14,24 @@ class Game
     @currentPlayer = @o.name
     puts "You are #{@currentPlayer}!"
 
-    self.Question
+    question
   end
 
-  def Question params = ''
-    self.DrawTable
+  def question params = ''
+    draw_table
 
     puts "\n\nIt's #{params}#{@currentPlayer}'s turn!"
     puts 'Where would you like to move? (Ex: A1, A2, etc):'
 
-    self.CheckAnswer gets.chomp
+    check_answer gets.chomp
   end
 
-  def CheckAnswer answer
+  def check_answer answer
+
     #check is the answer is in correct format
     unless answer =~ /^[abcABC][123]$/
       puts "Woops something bad's going on in here!"
-      self.Question 'still '
+      question 'still '
     else
       #split answer into the row and column values
       row, column = answer.split ''
@@ -37,70 +39,84 @@ class Game
       column = column.to_i - 1
 
       #checkin if the place is already taken
-      unless self.CheckPlaceTaken row, column
-        #checkin is the game should continue
-        self.CheckResult
-      end
+      check_result unless check_place_taken row, column
     end
+
   end
 
-  def CheckResult
-    if self.GameOver
-      self.DrawTable
+  def check_result
+
+    if game_over
+      draw_table
       puts "\nYay! #{@currentPlayer} won the game"
-    elsif self.CheckDraw
-      self.DrawTable
+    elsif check_draw
+      draw_table
       puts 'Draw!'
     else
       #switching the player
       @currentPlayer = @currentPlayer == @x.name ? @o.name : @x.name
       @@counter += 1 #counting the turns
-      self.Question
+      question
     end
+
   end
 
-  def CheckDraw
+  def check_draw
+
     @@counter == 9 ? true : false
+
   end
 
-  def CheckPlaceTaken row, column
+  def check_place_taken row, column
     taken = false
+
     if @@table[row][column] == ' '
       @@table[row][column] = @currentPlayer
     else
       puts 'Sorry bro, the place is taken'
-      self.Question 'still '
+      question 'still '
       taken = true
     end
+
     taken
   end
 
-  def GameOver
+  def game_over
     key = @currentPlayer.ord * 3
     over = false
+
     #cheking rows
     @@table.each do |x|
-      over = true if self.GetKey(x) == key
+      over = true if get_key(x) == key
     end
+
     #cheking columns
     @@table.transpose.each do |x|
-      over = true if self.GetKey(x) == key
+      over = true if get_key(x) == key
     end
+
     #chekin diagonals
-    over = true if self.GetKey(@@table.flatten.values_at(2, 4, 6)) == key || GetKey(@@table.flatten.values_at(0, 4, 8)) == key
+    over = true if get_key(@@table.flatten.values_at(2, 4, 6)) == key ||
+        get_key(@@table.flatten.values_at(0, 4, 8)) == key
+
     over
   end
 
-  def GetKey param
+  def get_key param
+
     param.join.split('').map(&:ord).inject(:+)
+
   end
 
-  def DrawTable
+  def draw_table
     countLines = 0
+
     @@table.each do |item|
       print item * '|'
       countLines += 1
       puts "\n-----" if countLines < 3
     end
+
   end
+
 end
